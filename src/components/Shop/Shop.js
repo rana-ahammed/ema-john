6aa3cd27de0482bf60cbfd7604e3f57186005fc1
ranaahammed012 <98573219+ranaahammed012@users.noreply.github.com>
2022-09-01 19:42:12 +0600

    upto 50/3 done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,26 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { fakeData } from "../../fakeData/products";
 import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
-  const first10 = fakeData.slice(0, 10);
-  const [products, setProducts] = useState(first10);
+  // const first10 = fakeData.slice(0, 10);
+  const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+    .then(res => res.json())
+    .then(data => setProducts(data))
+  }, [])
 
   useEffect(() => {
     const savedCart = getStoredCart();
     const productKeys = Object.keys(savedCart);
-    const previousCart = productKeys.map(existingKey => {
-      const product = fakeData.find(pd => pd.key === existingKey);
-      product.quantity = savedCart[existingKey];
-      return product;
-    })
+    console.log(products, productKeys);
+    if(products.length > 0){
+      const previousCart = productKeys.map(existingKey => {
+        const product = products.find(pd => pd.key === existingKey);
+        product.quantity = savedCart[existingKey];
+        return product;
+      })
     setCart(previousCart);
-  }, [])
+    }
+  }, [products])
 
   const handleAddProduct = (product) => {
     const toBeAddedKey = product.key;
