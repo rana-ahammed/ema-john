db4689e0d9b2c47c86e4b470f3e1dd830148f377
ranaahammed012 <98573219+ranaahammed012@users.noreply.github.com>
2022-09-02 18:25:03 +0600

    upto 50/6 done

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -2,13 +2,31 @@ import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { useInRouterContext } from 'react-router-dom';
 import { userContext } from '../../App';
+import { clearTheCart, getStoredCart } from '../../utilities/fakedb';
 import './Shipment.css';
 
 const Shipment = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const [loggedInUser, setLoggedInUser] = useContext(userContext);
   const onSubmit = data => {
-    console.log('form submitted', data)
+    const savedCart = getStoredCart();
+    const orderDetails = {...loggedInUser, products: savedCart, shipment: data, orderTime: new Date()};
+
+    fetch("http://localhost:5000/addOrder", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(orderDetails)
+    })
+    .then(res => res.json())
+    .then(data => {
+      if(data) {
+        clearTheCart();
+        alert("Your Order Placed Successfully")
+      }
+    })
+
   };
 
   console.log(watch("example"));
